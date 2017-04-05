c8553d9e9730319407362b33830db76fb9b07f08
speckhard <dts@stanford.edu>
2017-04-05 12:51:41 -0700

    updated eight node example

--- examples/__init__.py
+++ examples/__init__.py
@@ -9,4 +9,3 @@ Created on Tue Mar  7 22:21:28 2017
 This package includes exmaples to estimate the grid topology using statistical
 methods. 
 """
-
--- examples/eight_node_test.py
+++ examples/eight_node_test.py
@@ -7,23 +7,25 @@ Created on Fri Mar 31 08:43:21 2017
 
 This script acts as an example to run the mutual information based grid
 estimation aglorithm developed in the Sustainable Systems Lab at Stanford
-University on a 8 bus IEEE network. 
+University on a 8 bus IEEE network.
 """
 import numpy as np
 from scipy.io import loadmat
+# This allows us to run the example as a main file and not as a package.
+# It takes care of relative importing in python. 
+if __name__ == '__main__' and __package__ is None:
+    from os import sys, path
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__)))) 
 from estimation.grid_est import GridEst
-
 # 8 Node Example.
-matfile = loadmat('/Users/Dboy/Downloads/Node8_randPF_solar.mat',
+matfile = loadmat('Node8_randPF_solar.mat',
                   squeeze_me=True, struct_as_record=False)
-
 # Load the voltage magnitude matrix.
 vmag_matrix = matfile['v_vec']
 # Remove the feeder Node 1 data from v_vec.
 vmag_matrix = np.delete(vmag_matrix, 0, 1)
 # Load the ture branches of the network.
 mpc_base = matfile['mpc_base']
-
 # Take the columns corresponding to true branches.
 # Subtract by 1, to account for feeder node being deleted.
 true_branches = mpc_base.branch[:,0:2]
@@ -33,7 +35,7 @@ true_branches = np.delete(true_branches, 0, 0 ) - 1
 # Create a GridEst objec to prepare to run the algorithm.
 node8_randPF_solar = GridEst(true_branches, vmag_matrix, 'Node8_randPF_solar')
 # Choose mutual information method, either gaussian, MLE, sk_discrete, or JVHW.
-mi_method = 'MLE'
+mi_method = 'gaussian'
 node8_randPF_solar.run_mi_alg(mi_method)
 # Print successful detection rate of algorithm.
 print('The SDR is: ' + str(node8_randPF_solar.find_sdr()[0]) + '%')
