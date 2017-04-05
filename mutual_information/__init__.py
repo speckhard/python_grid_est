247c0cf90031edfb80447057ba5940a473f43bd4
speckhard <dts@stanford.edu>
2017-04-05 08:34:51 -0700

    package for mutual information calculation

---
+++ mutual_information/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 31 04:39:06 2017
+
+@author: Daniel Speckhard, DTS@stanford.edu
+
+This module contains methods relating to calculating the mutual information
+between two distributions.
+"""
+
+import gaussian
+import discrete
+import jvhw
