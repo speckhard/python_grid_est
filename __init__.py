41b526f0aad284561a1d20c0e9392ae9f47eb8d7
speckhard <dts@stanford.edu>
2017-04-05 08:29:03 -0700

    highest-level package

---
+++ __init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+
+Created on Tue Mar  7 22:21:28 2017
+
+@author: Daniel Speckhard, DTS@stanford.edu
+
+This package includes exmaples to estimate the grid topology using statistical
+methods. 
+"""
+
