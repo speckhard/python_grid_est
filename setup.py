3bea71a60e28254833e14425a69218296676b1de
speckhard <dts@stanford.edu>
2017-04-18 10:27:56 -0700

    files for pip install

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
 setup(
-  name = 'grid_top_est',
+  name = 'gridtopologyestimation',
   packages = ['estimation', 'examples', 'mutual_information', 'transform_data'],
-  version = '0.1',
+  version = '0.4',
   description = 'Library to estimate topology of electrical grid using voltage sensor data',
   author = 'Daniel Speckhard',
   author_email = 'dts@stanford.edu',
   url = 'https://github.com/speckhard/Python_Grid_Est', # use the URL to the github repo
-  download_url = 'https://github.com/speckhard/Python_Grid_Est/archive/0.1.tar.gz', 
+  download_url = 'https://github.com/speckhard/Python_Grid_Est/archive/0.4.tar.gz', 
   keywords = ['information-theory', 'smart-grid', 'estimation', 'toplogy', 'mutual-information'], 
   classifiers = [],
+  zip_safe = False,
 )
