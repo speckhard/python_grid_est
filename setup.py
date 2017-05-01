20bf12835af5e44ddfffd89637c182ba8c0e6fd6
speckhard <dts@stanford.edu>
2017-04-30 18:07:17 -0700

    v0dot9

--- setup.py
+++ setup.py
@@ -1,14 +1,28 @@
 from distutils.core import setup
-setup(
-  name = 'discretetopology',
-  packages = ['discretetopology'],
-  version = '0.8',
-  description = 'Library to estimate topology of electrical grid using voltage sensor data',
-  author = 'Daniel Speckhard',
-  author_email = 'dts@stanford.edu',
-  url = 'https://github.com/speckhard/Python_Grid_Est', # use the URL to the github repo
-  download_url = 'https://github.com/speckhard/Python_Grid_Est/archive/0.8.tar.gz', 
-  keywords = ['information-theory', 'smart-grid', 'estimation', 'toplogy', 'mutual-information'], 
-  classifiers = [],
-  zip_safe = False,
-)
+from distutils import util
+if __name__ == "__main__":
+    pathMySubPackage1 = util.convert_path('discretetopology/estimation')
+    pathMySubPackage2 = util.convert_path('discretetopology/mutual_information')
+    pathMySubPackage3 = util.convert_path('discretetopology/transform_data')
+    setup(
+        name='discretetopology',
+        package_dir = {
+            'discretetopology': 'discretetopology',
+            'discretetopology.estimation': pathMySubPackage1,
+            'discretetopology.mutual_infromation': pathMySubPackage2,
+            'discretetopology.tranform_data': pathMySubPackage3},
+        packages=['myPackage', 'discretetopology.estimation',
+                  'discretetopology.mutual_infromation',
+                  'discretetopology.tranform_data']
+        name = 'discretetopology',
+        packages = ['discretetopology'],
+        version = '0.9',
+        description = 'Library to estimate topology of electrical grid using voltage sensor data',
+        author = 'Daniel Speckhard',
+        author_email = 'dts@stanford.edu',
+        url = 'https://github.com/speckhard/Python_Grid_Est', # use the URL to the github repo
+        download_url = 'https://github.com/speckhard/Python_Grid_Est/archive/0.9.tar.gz', 
+        keywords = ['information-theory', 'smart-grid', 'estimation', 'toplogy', 'mutual-information'], 
+        classifiers = [],
+        zip_safe = False,
+    )
