a3d12e4c40a0b56a3bec5d7aec855960d285832e
speckhard <dts@stanford.edu>
2017-04-30 17:35:44 -0700

    modified for new directory structure.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'gridtopologyestimation',
-  packages = ['estimation', 'examples', 'mutual_information', 'transform_data'],
-  version = '0.4',
+  name = 'discretetopology',
+  packages = ['discretetopology'],
+  version = '0.5',
   description = 'Library to estimate topology of electrical grid using voltage sensor data',
   author = 'Daniel Speckhard',
   author_email = 'dts@stanford.edu',
   url = 'https://github.com/speckhard/Python_Grid_Est', # use the URL to the github repo
-  download_url = 'https://github.com/speckhard/Python_Grid_Est/archive/0.4.tar.gz', 
+  download_url = 'https://github.com/speckhard/Python_Grid_Est/archive/0.5.tar.gz', 
   keywords = ['information-theory', 'smart-grid', 'estimation', 'toplogy', 'mutual-information'], 
   classifiers = [],
   zip_safe = False,
